09915fc38add20856bead7af7d639edf1cc54351
Jeremy Glover <jeremyglover33@gmail.com>
2015-11-17 23:27:34 -0600

    Set up cancel form, attemped to pull images from server

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ export default function() {
     $(`.image-form`).slideToggle();
   });
 
+  var showInfo = function(info) {
+    $(`<img></img>`).text(`${info.image}`).appendTo(`.image-list`);
+    $(`<p></p>`).text(`${info.caption}`).appendTo(`.image-list`);
+  };
+
   $(`.image-form`).on(`submit`, function(ev) {
 
     ev.preventDefault();
@@ -21,6 +26,21 @@ export default function() {
       $(`#form-url`).val(' ');
       $(`#form-caption`).val(' ');
 
+      showInfo(response);
   });
 });
+
+  $(`.cancel`).on(`click`, function() {
+    $(`.image-form`).slideUp;
+    $(`#form-url`).val(' ');
+    $(`#form-caption`).val(' ');
+  })
+
+  $.ajax({
+    url: `http://tiny-lr.herokuapp.com/collections/photos-jg`,
+    method: `GET`,
+    dataType: `json`,
+  }).then((response) => {
+    response.forEach(showInfo);
+  });
 }
