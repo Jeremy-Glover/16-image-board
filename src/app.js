bd3a0161d84db08dbdc3ba1271f559dad5a78f3d
Jeremy Glover <jeremyglover33@gmail.com>
2015-11-17 22:02:50 -0600

    Submit images and slide form up

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-export default function() {}
+export default function() {
+
+  $(`.nav-board`).on(`click`, function() {
+    $(`.image-form`).slideToggle();
+  });
+
+  $(`.image-form`).on(`submit`, function(ev) {
+
+    ev.preventDefault();
+
+    var image = $(`#form-url`).val();
+    var caption = $(`#form-caption`).val();
+
+    $.ajax({
+      url: `http://tiny-lr.herokuapp.com/collections/photos-jg`,
+      method: `POST`,
+      dataType: `json`,
+      data: {image, caption},
+    }).then((response) => {
+      $(`.image-form`).slideUp();
+      $(`#form-url`).val(' ');
+      $(`#form-caption`).val(' ');
+
+  });
+});
+}
